4721fcd12b4919f6f300367f6a86dc929aab5a7a
Timo Adameit <timo95@users.noreply.github.com>
2019-10-23 17:29:31 +0200

    Start change to multi project

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,10 @@
+
+object Versions {
+    var self = "0.0.1-SNAPSHOT"
+
+    var scalaFull = "2.12.10"
+    var scalaMajor = "2.12"
+
+    var spark = "2.4.4"
+    var scalatest = "3.0.8"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,5 @@
  */
 
 rootProject.name = "gradoop-spark"
+
+include("gradoop-spark", "gradoop-spark-common")
